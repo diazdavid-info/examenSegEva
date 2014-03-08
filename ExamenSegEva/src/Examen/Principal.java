48eaec4349599422e865c0f8e31bb08c62f5a62f
David <david.diaz@example.org>
2014-03-08 10:05:50 +0100

    Primer Commit

---
+++ ExamenSegEva/src/Examen/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Examen;
+
+/**
+ * @author David
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
